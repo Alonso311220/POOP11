0eeb3ed21cd605277c652f717f0f7882b705391d
Alonso311220 <129483389+Alonso311220@users.noreply.github.com>
2023-06-01 22:21:49 -0600

    Adición de javadoc de la actividad

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poop11"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"poop11_actividad"}];updateSearchResults();
